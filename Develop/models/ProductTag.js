efc82129580e0f2e5b77351da5ae87266f7a5007
Saraf Nawar <s.999895831@gmail.com>
2023-03-19 01:19:20 -0400

    access denied

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,30 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define id & product id columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // reference model product id
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    //define tag id columns
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //reference model tag id
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,27 +2,59 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+//instead of try catch trying then catch 
+
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product],
+  })
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => res.status(404).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((dbCategoryData) => res.status(200).json(dbCategoryData))
+  .catch((err) => res.status(404).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbCategoryData) => res.status(200).json(dbCategoryData))
+  .catch((err) => res.status(404).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((dbCategoryData) => res.status(200).json(dbCategoryData))
+  .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
